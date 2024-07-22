1dcbd18ed09a0651276e4af473d27bbfdb680365
IrshadRaja06 <142392485+IrshadRaja06@users.noreply.github.com>
2024-07-22 16:37:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function wrong1(){
+    document.getElementById("btn1").style.background="red";
+}
+function wrong2(){
+    document.getElementById("btn2").style.background="red";
+}
+function wrong3(){
+    document.getElementById("btn3").style.background="red";
+}
+function correct(){
+    document.getElementById("btn").style.background="green";
+}
